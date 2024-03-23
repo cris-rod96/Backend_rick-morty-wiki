4fc69fa1535f09f067bfcbfb3c38578b7a784536
Cristhian Rodríguez <crisrodam1996@gmail.com>
2024-03-23 18:56:59 -0500

    Last Configuration

--- src/config/index.config.js
+++ src/config/index.config.js
@@ -1,3 +1,3 @@
 import { config } from "dotenv";
 config();
-export const { HOST_PORT, DATABASE_URI, SECRET_KEY } = process.env;
+export const { HOST_PORT, DATABASE_URI, SECRET_KEY, DB_DEPLOY } = process.env;
