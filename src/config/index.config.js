e120fd09eef1c1edda244183bf29313e5afbf316
Cristhian Rodríguez <crisrodam1996@gmail.com>
2024-03-23 22:12:06 -0500

    Cors Configuration v6

--- src/config/index.config.js
+++ src/config/index.config.js
@@ -1,3 +1,10 @@
 import { config } from "dotenv";
 config();
-export const { HOST_PORT, DATABASE_URI, SECRET_KEY, DB_DEPLOY } = process.env;
+export const {
+  HOST_PORT,
+  DATABASE_URI,
+  SECRET_KEY,
+  DB_DEPLOY,
+  ORIGIN_ACCEPT_URL_DEV,
+  ORIGIN_ACCEPT_URL_DEPLOY,
+} = process.env;
--- src/server.js
+++ src/server.js
@@ -1,20 +1,22 @@
 import express from "express";
 import morgan from "morgan";
+import bodyParser from "body-parser";
 import routes from "./routes/index.routes.js";
 
 const server = express();
 
 // Middlewares
+server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(express.json());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader(
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
     "Access-Control-Allow-Headers",
     "Origin,X-Requested-With,Content-Type,Accept"
   );
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
   next();
 });
 // server.use("/api/v1", routes);
