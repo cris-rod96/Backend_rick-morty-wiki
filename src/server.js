8a0aaea8941d5522eb157808398113ad7337df6b
Cristhian Rodríguez <crisrodam1996@gmail.com>
2024-03-23 19:31:19 -0500

    CHANGE MAIN MIDDLEWARE

--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ import cors from "cors";
 import morgan from "morgan";
 import routes from "./routes/index.routes.js";
 
-
 const server = express();
 
 // Middlewares
@@ -11,6 +10,7 @@ server.use(express.json());
 server.use(cors());
 server.use(morgan("dev"));
 
-server.use("/api/v1", routes);
+// server.use("/api/v1", routes);
+server.use("/", routes);
 
 export default server;
