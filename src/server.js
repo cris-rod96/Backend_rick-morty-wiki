b91518d9aa21d11bc7d69c94318a026887960ef9
Cristhian Rodríguez <crisrodam1996@gmail.com>
2024-03-23 20:09:03 -0500

    Cors Configuration v3

--- src/server.js
+++ src/server.js
@@ -8,13 +8,13 @@ const server = express();
 server.use(express.json());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin,X-Requested-With,Content-Type,Accept"
   );
-  res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS,PUT,DELETE");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS,PUT,DELETE");
   next();
 });
 // server.use("/api/v1", routes);
