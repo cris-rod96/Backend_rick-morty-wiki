86440cd407d375ac68a8478ef056303a35adfb91
Cristhian Rodríguez <crisrodam1996@gmail.com>
2024-03-23 19:50:06 -0500

    Cors Configuration

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 import express from "express";
-import cors from "cors";
 import morgan from "morgan";
 import routes from "./routes/index.routes.js";
 
@@ -7,9 +6,17 @@ const server = express();
 
 // Middlewares
 server.use(express.json());
-server.use(cors());
 server.use(morgan("dev"));
-
+server.use((req, res, next) => {
+  res.header("Access-Controll-Allow-Origin", "*");
+  res.header("Access-Controll-Allow-Credentials", "true");
+  res.header(
+    "Access-Controll-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept"
+  );
+  res.header("Access-Controll-Allow-Methods", "GET,POST,OPTIONS,PUT,DELETE");
+  next();
+});
 // server.use("/api/v1", routes);
 server.use("/", routes);
 
